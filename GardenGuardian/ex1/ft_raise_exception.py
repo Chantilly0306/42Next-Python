16970d8eb34a8a757f472bce8ea47c5c0dee39e2
Chantilly0306 <miffy7215@gmail.com>
2026-08-24 18:09:14 +0200

    raise error

--- GardenGuardian/ex1/ft_raise_exception.py
+++ GardenGuardian/ex1/ft_raise_exception.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-def input_temperature(tmp: str) -> int:
-    return int(tmp)
+def input_temperature(tmp_str: str) -> int:
+    tmp: int = int(tmp_str)
+    if tmp > 40:
+        raise ValueError(f"{tmp}°C is too hot for plants (max 40°C)")
+    if tmp < 0:
+        raise ValueError(f"{tmp}°C is too cold for plants (min 0°C)")
+    return tmp
 
 
 def test_temperature() -> None:
-    print("=== Garden Temperature ===")
+    print("=== Garden Temperature Checker ===")
 
-    test_inputs: list[str] = ["25", "abc"]
+    test_inputs: list[str] = ["25", "abc", "100", "-50"]
 
     for data in test_inputs:
         print(f"Input data is '{data}'")
