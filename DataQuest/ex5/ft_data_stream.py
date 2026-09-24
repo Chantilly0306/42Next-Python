94f9dfe743221dcbed7b73bc13a86b303b50ce55
“Hsiang <miffy7215@gmail.com>
2026-09-24 09:57:15 +0200

    ex5

---
+++ DataQuest/ex5/ft_data_stream.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python3
+from typing import Generator
+import random
+
+
+PLAYERS = ["alice", "bob", "charlie", "dylan"]
+ACTIONS = ["run", "eat", "sleep", "grab", "move", 
+           "climb", "swim", "release", "grab"]
+
+
+def gen_event():
+    
+    
+def consume_event(event: list[tuple[str, str]]):
+
+
+def main()
+
+
+if __name__ == "__main__":
+    main()
+    
